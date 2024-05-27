78eb06d2fd6e37a45373bd98dfdc00d2b62479b3
Power70 <nwisuanu@gmail.com>
2024-05-27 20:55:16 +0100

    added registration and login form

--- flask_blog.py
+++ flask_blog.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistratonForm, LoginForm
 app = Flask(__name__)
 
+# add secrete key
+app.config["SECRET_KEY"] = "27b6dcd360704809fc675ebe7db64540"
 posts = [
     {
         "author": "Torzor Hub",
@@ -23,6 +26,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title="About")
+@app.route("/register")
+def register():
+    form = RegistratonForm() #this create an instance of the form
+    return render_template("register.html", title="Register", form=form)
+    # with form=form we have access to the instance above
+@app.route("/login")
+def login():
+    form = LoginForm() #this create an instance of the form
+    return render_template("login.html", title="Login", form=form)
+    # with form=form we have access to the instance above
 
 if __name__ == "__main__":
     app.run(debug=True)
